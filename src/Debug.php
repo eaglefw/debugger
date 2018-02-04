50dd92416514c8223c19e91dee513419526e908b
milankyncl <milan.kyncl@icloud.com>
2018-02-04 10:55:59 +0100

    structure change

--- src/Debug.php
+++ src/Debug.php
@@ -4,14 +4,30 @@
 namespace Eagle;
 
 use Phalcon\Debug as PhalconDebug;
+use MilanKyncl\Debugbar\PhalconDebugbar;
 
 /**
  * Class Debug
  * @package Eagle
  */
 
-class Debug extends PhalconDebug{
+class Debug extends PhalconDebug {
 
-	public $_uri = "//static.phalconphp.com/www/debug/3.0.x/";
+	public $_uri = 'https://eaglefw.github.io/debugger/';
+
+
+	public function __construct($mode = self::OFF) {
+
+		$debugbar = new PhalconDebugbar();
+		$debugbar->setDebugMode($mode);
+
+		if($debugbar->isDebugMode())
+			$this->listen(true, true);
+
+	}
+
+	const ON = true;
+
+	const OFF = false;
 
 }
